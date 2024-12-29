a79d3cdcf5cde8d852e4cca3e793f0e2b9bc9490
Kyrylo Bulyhin <kbulygin456@gmail.com>
2024-12-29 18:26:04 +0200

    fix (style): change price on 'PAID' sign if course already booked by user

--- resources/views/components/courses/index.blade.php
+++ resources/views/components/courses/index.blade.php
@@ -1,7 +1,7 @@
-@props(['courses' => []])
+@props(['courses' => [], 'userBookedCourses' => []])
 
 <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
     @foreach ($courses as $item)
-        <x-recent-course :course="$item" />
+        <x-recent-course :course="$item" :is-booked="in_array($item->id, $userBookedCourses)" />
     @endforeach
 </ul>
